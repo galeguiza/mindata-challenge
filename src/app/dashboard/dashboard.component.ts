042309a33bc2fc45b3730f4f7f55f18e339fb6b6
Gabriel Leguizamon <gabrielleguizamon.developer@gmail.com>
2025-02-19 18:16:05 -0300

    Updated dashboard to display componentes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CardContentComponent } from '../shared/components/card-content/card-content.component';
+import { CardUserListComponent, User } from '../shared/components/card-user-list/card-user-list.component';
+import { CardUserProfileComponent } from '../shared/components/card-user-profile/card-user-profile.component';
+import { CardProductComponent, Product } from '../shared/components/card-product/card-product.component';
+import { USER_MOCK } from '../shared/mocks/userMock';
+import { PRODUCT_MOCK } from '../shared/mocks/productMock';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CardUserListComponent, CardUserProfileComponent, CardProductComponent, CardContentComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-
+  public users: User[] = USER_MOCK;
+  public product: Product = PRODUCT_MOCK;
 }
