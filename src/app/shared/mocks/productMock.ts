f0d2cced6316eca42f533392aba6955f64b78a8b
Gabriel Leguizamon <gabrielleguizamon.developer@gmail.com>
2025-02-19 18:12:13 -0300

    Added global styles and variables

---
+++ src/app/shared/mocks/productMock.ts
@@ -0,0 +1,7 @@
+export const PRODUCT_MOCK = {
+  name: 'Strawberry Cake',
+  productImg: 'product-img.png',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+  price: 13.90,
+  oldPrice: 18.90
+};
